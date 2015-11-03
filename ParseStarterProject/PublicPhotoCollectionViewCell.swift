ed363dfaf1cc82d7fdda65680da92f580e42aa36
Eric Smith <ericsmith1302@gmail.com>
2015-11-03 03:46:41 -0500

    Fixed some issues with image icons not rounding after going back to the view

--- ParseStarterProject/PublicPhotoCollectionViewCell.swift
+++ ParseStarterProject/PublicPhotoCollectionViewCell.swift
@@ -14,15 +14,18 @@ class PublicPhotoCollectionViewCell: PFCollectionViewCell {
 
     @IBOutlet weak var pfImageView: CachedPFImageView!
     
+    // Need this functionality to update the frame as soon as the bound is set to the imageView can calculate the corner radius before rendering
+    override var bounds: CGRect {
+        didSet {
+            contentView.frame = bounds
+        }
+    }
+    
     func configure(file:PFFile) {
         pfImageView.file = file;
-        // Need to figure out a fix for this. Configure is called before the cell's frame is made. So the imageview frame is wrong thus we cant
-        // set the corner radius properly. Waiting until the load is done works, but the load isnt always triggered if the image is cached.
-        // Need to figure out when to call this method after everything is setup.
-        pfImageView.loadInBackground({
-            (image, error) in
-            self.pfImageView.layer.cornerRadius = (self.pfImageView.frame.size.height)/2
-        })
+        pfImageView.loadInBackground()
+        pfImageView.layoutIfNeeded()
+        self.pfImageView.layer.cornerRadius = (self.pfImageView.frame.size.height)/2
         pfImageView.layer.masksToBounds = true
         pfImageView.layer.borderColor = UIColor(red: 0.834, green: 0.978, blue: 1.000, alpha: 0.4).CGColor
         pfImageView.layer.borderWidth = 5
